b0930b9ed5f931158a7b56ce4c297d9cad1aef86
Nurit Dor <57101353+nd-certora@users.noreply.github.com>
2022-04-27 10:31:58 +0300

    bytecode example

---
+++ AAVE/ERC20/bytecode/verifyBytecode.sh
@@ -0,0 +1,7 @@
+certoraRun --bytecode DoYouTrustMe1.json --bytecode_spec erc20.spec \
+    --staging  \
+    --msg "doYouTrustMe1:erc20.spec  "
+certoraRun --bytecode DoYouTrustMe2.json --bytecode_spec erc20.spec \
+    --staging  \
+    --msg "doYouTrustMe2:erc20.spec  "
+
