cdd8e44ac9632fb8a1647d91fe61dbef85022235
Yura Sherman <yurasherman+github@gmail.com>
2022-04-28 17:33:45 +0300

    add ReserveList example

---
+++ 3DayWorkshop/PracticeBugFinding/ReserveList/IReserveList.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8.7;
+
+interface IReserveList {
+    
+    /**
+     * @dev Details on assets in reserve mapping
+     * @param id - a unique id generated for the asset. 
+     * @note that the given id to a reserve asset is its location (index) in the underlying map
+     * @param token - the token address
+     * @param fee - a constant fee associated with trading this asset
+     */ 
+    struct ReserveData {
+        uint256 id;
+        address lpToken;
+        address stableDebtToken;
+        address varDebtToken;
+        uint256 fee;
+    }
+
+    // Gets the token saved in underlyingList mapping according to input index
+    function getTokenAtIndex(uint256 index) external view returns (address);
+
+    // Gets the ID saved in reserved mapping according to input token id
+    function getIdOfToken(address token) external view returns (uint256);
+
+    // Gets the count of underlying assets in the list
+    function getReserveCount() external view returns (uint256);
+
+    // Adds a reserve to the list and updates its details
+    function addReserve(address token, address stableToken, address varToken, uint256 fee) external;
+
+    // Removes a specified reserve from the list.
+    function removeReserve(address token) external;
+}
---
+++ 3DayWorkshop/PracticeBugFinding/ReserveList/solution/runCross.sh
@@ -0,0 +1,4 @@
+certoraRun ReserveListFixed.sol:ReserveList --verify ReserveList:Reserve.spec \
+--solc solc8.7 \
+--optimistic_loop \
+--loop_iter 3 \
