d07e91ab5a3c75cd1c3fb78b52c16e10f08837f4
Nurit Dor <57101353+nd-certora@users.noreply.github.com>
2022-07-22 11:24:48 +0200

    rule flag

--- EthCC/LiquidityPool/run.sh
+++ EthCC/LiquidityPool/run.sh
@@ -1,6 +1,12 @@
+if [[ "$1" ]]
+then
+    RULE="--rule $1"
+fi
+
 certoraRun LiquidityPool.sol Asset_ERC20.sol SymbolicFlashLoanReceiver.sol \
     --link LiquidityPool:asset=Asset_ERC20 \
 	--verify LiquidityPool:pool.spec \
     --solc solc8.0 \
     --cloud  \
+    $RULE \
     --msg "Liquidity Pool:  $1 " \
