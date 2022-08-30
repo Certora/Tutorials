1df19d09006a4971b9c359562bb3726312204cb2
Yura Sherman <yurasherman+github@gmail.com>
2022-08-30 14:12:52 +0000

    fix run.sh

--- eth-warsaw/run.sh
+++ eth-warsaw/run.sh
@@ -1,11 +1,5 @@
-if [[ "$1" ]]
-then
-    RULE="--rule $1"
-fi
-
 certoraRun contracts/ERC20.sol:ERC20 \
     --verify ERC20:setup.spec \
     --optimistic_loop \
-    $RULE \
     --msg "ERC20:setup.spec $1"
  
