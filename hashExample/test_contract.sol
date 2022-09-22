4aa8f488e313e90c76c472f6e5ba9ceb3cc33175
ALEX JOSEPH <alexjoseph@ALEXs-MacBook-Pro.local>
2022-09-22 14:09:40 +0100

    updated spec

--- hashExample/test_contract.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.6;
-
-contract Example {
-  function example(bytes32 target, bytes calldata witness) public pure returns (uint256) {
-    require(target == keccak256(witness), "BAD_WITNESS");
-    return witness.length;
-  }
-}
