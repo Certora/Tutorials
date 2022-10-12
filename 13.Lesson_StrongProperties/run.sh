b4c9abe6651acd9fd3cca948accf547523b2fa6f
Nurit Dor <57101353+nd-certora@users.noreply.github.com>
2022-10-12 22:45:47 +0300

    lesson 13 - properties

---
+++ 13.Lesson_StrongProperties/run.sh
@@ -0,0 +1,8 @@
+certoraRun EnglishAuction.sol DummyERC721.sol --verify EnglishAuction:EnglishAuction.spec \
+--solc solc8.13  --link EnglishAuction:nft=DummyERC721 --settings -optimisticFallback=true  \
+--msg "EnglishAuction $1" 
+#
+# overcoming calls to EOAs that are not easily linked (essentialy a dispatcher logic for ETH-sending only calls).
+# --settings -optimisticFallback=true  
+# linking the nft to a apecific contract 
+#--link EnglishAuction:nft=DummyERC721 
